fe4b7dd36542557034954058e590ed575d0df232
Mersant <dlowry256@outlook.com>
2024-09-17 21:41:38 +0000

    Set tab size to 4 by default

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.autoformat = false
+
+local set = vim.opt
+set.tabstop = 4
+set.shiftwidth = 4
+set.expandtab = true
