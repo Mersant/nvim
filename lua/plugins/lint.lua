566cd84e729316a58d155685b6923bd91c4af4b4
David Lowry <dlowry@revlocal.com>
2024-09-18 10:42:04 -0400

    Make lint.lua cross platform

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,5 +1,13 @@
--- ~/.config/nvim/lua/plugins/lint.lua
-local HOME = os.getenv("HOME")
+local function get_config_path()
+  local home = os.getenv("HOME") or os.getenv("USERPROFILE")
+  local config_file = "/.markdownlint-cli2.yaml"
+
+  if vim.fn.has("win32") == 1 then
+    return home .. config_file:gsub("/", "\\")
+  else
+    return home .. config_file
+  end
+end
 
 return {
   {
@@ -7,10 +15,9 @@ return {
     opts = {
       linters = {
         ["markdownlint-cli2"] = {
-          args = { "--config", HOME .. "/.markdownlint-cli2.yaml","--" },
+          args = { "--config", get_config_path(), "--" },
         },
       },
     },
   },
 }
-
